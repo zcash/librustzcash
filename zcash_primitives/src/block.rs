e21be37042bc2a56ec44e1af8fa2eb3db75d3546
Jack Grigg <jack@z.cash>
2018-10-11 23:14:46 +0100

    Block header representation

---
+++ zcash_primitives/src/block.rs
@@ -0,0 +1,32 @@
+use std::ops::Deref;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct BlockHash(pub [u8; 32]);
+
+/// A Zcash block header.
+pub struct BlockHeader(BlockHeaderData);
+
+impl Deref for BlockHeader {
+    type Target = BlockHeaderData;
+
+    fn deref(&self) -> &BlockHeaderData {
+        &self.0
+    }
+}
+
+pub struct BlockHeaderData {
+    pub version: i32,
+    pub prev_block: BlockHash,
+    pub merkle_root: [u8; 32],
+    pub final_sapling_root: [u8; 32],
+    pub time: u32,
+    pub bits: u32,
+    pub nonce: [u8; 32],
+    pub solution: Vec<u8>,
+}
+
+impl BlockHeaderData {
+    pub fn freeze(self) -> BlockHeader {
+        BlockHeader(self)
+    }
+}
