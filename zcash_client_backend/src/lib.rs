16a49a969e2f6e6bfc73274f05df57566a20e77f
Jack Grigg <jack@z.cash>
2018-10-22 17:36:23 +0100

    Rename zcash_wallet to zcash_client_backend, set to 2018 edition

---
+++ zcash_client_backend/src/lib.rs
@@ -0,0 +1,12 @@
+//! *A crate for implementing Zcash light clients.*
+//!
+//! `zcash_client_backend` contains Rust structs and traits for creating shielded Zcash
+//! light clients.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
