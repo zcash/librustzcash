fdac1b6a2ef684c1525cc145b0b4feaa1ddf2548
Jack Grigg <jack@z.cash>
2019-08-03 10:39:34 +0100

    ZIP 304 implementation

---
+++ zcash_primitives/src/sapling/zip304.rs
@@ -0,0 +1,351 @@
+//! [ZIP 304] protocol for signing arbitrary messages with Sapling payment addresses.
+//!
+//! [ZIP 304]: https://zips.z.cash/zip-0304
+
+use bellman::{
+    gadgets::multipack,
+    groth16::{verify_proof, Proof},
+};
+use group::{ff::Field, Curve};
+use rand_core::{CryptoRng, OsRng, RngCore};
+use std::convert::TryInto;
+
+use super::{
+    bundle::GrothProofBytes,
+    circuit::PreparedSpendVerifyingKey,
+    constants::SPENDING_KEY_GENERATOR,
+    keys::ExpandedSpendingKey,
+    prover::SpendProver,
+    redjubjub::{self, PrivateKey, PublicKey},
+    spend_sig,
+    tree::{CommitmentTree, IncrementalWitness},
+    value::{NoteValue, ValueCommitTrapdoor, ValueCommitment},
+    verify_spend_sig_internal, Node, Note, Nullifier, PaymentAddress, ProofGenerationKey, Rseed,
+};
+
+const ZIP304_PERSONALIZATION_PREFIX: &'static [u8; 12] = b"ZIP304Signed";
+
+fn message_digest(coin_type: u32, zkproof: &GrothProofBytes, message: &[u8]) -> [u8; 32] {
+    let mut personal = [0; 16];
+    personal[..12].copy_from_slice(ZIP304_PERSONALIZATION_PREFIX);
+    personal[12..].copy_from_slice(&coin_type.to_le_bytes());
+    blake2b_simd::Params::new()
+        .hash_length(32)
+        .personal(&personal)
+        .to_state()
+        .update(zkproof)
+        .update(message)
+        .finalize()
+        .as_bytes()
+        .try_into()
+        .unwrap()
+}
+
+/// A ZIP 304 signature over an arbitrary message, created with the spending key of a
+/// Sapling payment address.
+///
+/// A normal (and desired) property of signatures is that all signatures for a specific
+/// public key are linkable if the public key is known. ZIP 304 signatures have the
+/// additional property that all signatures for a specific payment address are linkable
+/// without knowing the payment address, as the first 32 bytes of each signature will be
+/// identical.
+///
+/// A signature is bound to a specific diversified address of the spending key. Signatures
+/// for different diversified addresses of the same spending key are unlinkable.
+pub struct Signature {
+    nullifier: Nullifier,
+    rk: redjubjub::PublicKey,
+    zkproof: GrothProofBytes,
+    spend_auth_sig: redjubjub::Signature,
+}
+
+impl Signature {
+    pub fn from_bytes(bytes: &[u8; 320]) -> Option<Self> {
+        let nullifier = Nullifier(bytes[0..32].try_into().unwrap());
+
+        let rk = match PublicKey::read(&bytes[32..64]) {
+            Ok(p) => p,
+            Err(_) => return None,
+        };
+        if rk.0.is_small_order().into() {
+            return None;
+        }
+
+        let mut zkproof = [0; 192];
+        zkproof.copy_from_slice(&bytes[64..256]);
+
+        let spend_auth_sig = match redjubjub::Signature::read(&bytes[256..320]) {
+            Ok(sig) => sig,
+            Err(_) => return None,
+        };
+
+        Some(Signature {
+            nullifier,
+            rk,
+            zkproof,
+            spend_auth_sig,
+        })
+    }
+
+    pub fn to_bytes(&self) -> [u8; 320] {
+        let mut bytes = [0; 320];
+        bytes[0..32].copy_from_slice(&self.nullifier.0);
+        self.rk.write(&mut bytes[32..64]).unwrap();
+        bytes[64..256].copy_from_slice(&self.zkproof);
+        self.spend_auth_sig.write(&mut bytes[256..320]).unwrap();
+        bytes
+    }
+}
+
+/// Signs an arbitrary message for the given [`PaymentAddress`] and [`SLIP 44`] coin type.
+///
+/// The coin type is used here in its index form, not its hardened form (i.e. 133 for
+/// mainnet Zcash).
+///
+/// [`SLIP 44`]: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+pub fn sign_message<Pr: SpendProver>(
+    expsk: &ExpandedSpendingKey,
+    payment_address: PaymentAddress,
+    coin_type: u32,
+    message: &[u8],
+    prover: &Pr,
+) -> Signature {
+    // Initialize secure RNG.
+    let mut rng = OsRng;
+
+    // Derive the necessary key components.
+    let proof_generation_key = expsk.proof_generation_key();
+
+    // Re-randomize the payment address.
+    let alpha = jubjub::Scalar::random(&mut rng);
+    let rk = PublicKey(proof_generation_key.ak.into()).randomize(alpha, SPENDING_KEY_GENERATOR);
+
+    // Create the address proof.
+    let (nullifier, zkproof) = create_address_proof(
+        proof_generation_key,
+        payment_address,
+        alpha,
+        prover,
+        &mut rng,
+    );
+
+    // Compute the message digest to be signed.
+    let digest = message_digest(coin_type, &zkproof, message);
+
+    // Create the signature.
+    let spend_auth_sig = spend_sig(PrivateKey(expsk.ask), alpha, &digest, &mut rng);
+
+    Signature {
+        nullifier,
+        rk,
+        zkproof,
+        spend_auth_sig,
+    }
+}
+
+pub(crate) fn create_address_proof<Pr: SpendProver>(
+    proof_generation_key: ProofGenerationKey,
+    payment_address: PaymentAddress,
+    alpha: jubjub::Scalar,
+    prover: &Pr,
+    mut rng: impl RngCore + CryptoRng,
+) -> (Nullifier, GrothProofBytes) {
+    // We make a Sapling spend proof for a fake note with value of 1 zatoshi, setting rcm
+    // and rcv to zero.
+    let value = NoteValue::from_raw(1);
+    let rcm = jubjub::Scalar::zero();
+    let rcv = ValueCommitTrapdoor::zero();
+
+    // Create the fake note.
+    let note = Note::from_parts(payment_address, value, Rseed::BeforeZip212(rcm));
+
+    // Create a fake tree containing the fake note, and witness it.
+    let (anchor, witness) = {
+        let mut tree = CommitmentTree::empty();
+        tree.append(Node::from_cmu(&note.cmu())).unwrap();
+        (
+            tree.root(),
+            IncrementalWitness::from_tree(tree).path().unwrap(),
+        )
+    };
+
+    // Derive the nullifier for the fake note.
+    let nullifier = {
+        let vk = proof_generation_key.to_viewing_key();
+        note.nf(&vk.nk, witness.position().into())
+    };
+
+    // We now have the full witness for our circuit!
+    let instance = Pr::prepare_circuit(
+        proof_generation_key,
+        *payment_address.diversifier(),
+        Rseed::BeforeZip212(rcm),
+        value,
+        alpha,
+        rcv,
+        anchor.into(),
+        witness,
+    )
+    .expect("payment address is valid");
+
+    // Create the proof.
+    let zkproof = Pr::encode_proof(prover.create_proof(instance, &mut rng));
+
+    (nullifier, zkproof)
+}
+
+/// Verifies a [`Signature`] on a message with the given [`PaymentAddress`]  and
+/// [`SLIP 44`] coin type.
+///
+/// The coin type is used here in its index form, not its hardened form (i.e. 133 for
+/// mainnet Zcash).
+///
+/// [`SLIP 44`]: https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+pub fn verify_message(
+    payment_address: PaymentAddress,
+    coin_type: u32,
+    message: &[u8],
+    signature: &Signature,
+    verifying_key: &PreparedSpendVerifyingKey,
+) -> Result<(), ()> {
+    // Compute the message digest that was signed.
+    let digest = message_digest(coin_type, &signature.zkproof, message);
+
+    // Verify the spend_auth_sig.
+    if !verify_spend_sig_internal(&signature.rk, &digest, &signature.spend_auth_sig) {
+        return Err(());
+    }
+
+    // Verify the address proof.
+    verify_address_proof(
+        payment_address,
+        &signature.nullifier,
+        &signature.rk,
+        &signature.zkproof,
+        verifying_key,
+    )
+}
+
+pub(crate) fn verify_address_proof(
+    payment_address: PaymentAddress,
+    nullifier: &Nullifier,
+    rk: &redjubjub::PublicKey,
+    zkproof: &GrothProofBytes,
+    verifying_key: &PreparedSpendVerifyingKey,
+) -> Result<(), ()> {
+    // Parse the proof.
+    let zkproof = Proof::read(&zkproof[..]).map_err(|_| ())?;
+
+    // We created the proof for a fake note with value of 1 zatoshi, setting rcm and rcv
+    // to zero.
+    let value = NoteValue::from_raw(1);
+    let rcm = jubjub::Scalar::zero();
+    let rcv = ValueCommitTrapdoor::zero();
+
+    // Recreate the fake note.
+    let note = Note::from_parts(payment_address, value, Rseed::BeforeZip212(rcm));
+
+    // Recreate the fake tree containing the fake note.
+    let anchor = {
+        let mut tree = CommitmentTree::empty();
+        tree.append(Node::from_cmu(&note.cmu())).unwrap();
+        tree.root()
+    };
+
+    // Construct the value commitment.
+    let cv = ValueCommitment::derive(value, rcv);
+
+    // Construct public input for circuit.
+    let mut public_input = [bls12_381::Scalar::zero(); 7];
+    {
+        let affine = rk.0.to_affine();
+        let (u, v) = (affine.get_u(), affine.get_v());
+        public_input[0] = u;
+        public_input[1] = v;
+    }
+    {
+        let affine = cv.as_inner().to_affine();
+        let (u, v) = (affine.get_u(), affine.get_v());
+        public_input[2] = u;
+        public_input[3] = v;
+    }
+    public_input[4] = anchor.into();
+
+    // Add the nullifier through multiscalar packing.
+    {
+        let nullifier = multipack::bytes_to_bits_le(&nullifier.0);
+        let nullifier = multipack::compute_multipacking(&nullifier);
+
+        assert_eq!(nullifier.len(), 2);
+
+        public_input[5] = nullifier[0];
+        public_input[6] = nullifier[1];
+    }
+
+    // Verify the proof.
+    verify_proof(&verifying_key.0, &zkproof, &public_input[..]).map_err(|_| ())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::sapling::{
+        circuit::SpendParameters,
+        keys::ExpandedSpendingKey,
+        zip304::{sign_message, verify_message},
+        Diversifier,
+    };
+
+    #[test]
+    fn test_signatures() {
+        let (spend_buf, _) = wagyu_zcash_parameters::load_sapling_parameters();
+        let params = SpendParameters::read(&spend_buf[..], false)
+            .expect("Sapling parameters should be valid");
+        let spend_vk = params.prepared_verifying_key();
+
+        let expsk = ExpandedSpendingKey::from_spending_key(&[42; 32][..]);
+        let addr = {
+            let diversifier = Diversifier([0; 11]);
+            expsk
+                .proof_generation_key()
+                .to_viewing_key()
+                .to_payment_address(diversifier)
+                .unwrap()
+        };
+
+        let msg1 = b"Foo bar";
+        let msg2 = b"Spam eggs";
+
+        let sig1 = sign_message(&expsk, addr, 1, msg1, &params);
+        let sig2 = sign_message(&expsk, addr, 1, msg2, &params);
+
+        // The signatures are bound to the specific message they were created over
+        assert!(verify_message(addr, 1, msg1, &sig1, &spend_vk).is_ok());
+        assert!(verify_message(addr, 1, msg2, &sig2, &spend_vk).is_ok());
+        assert!(verify_message(addr, 1, msg1, &sig2, &spend_vk).is_err());
+        assert!(verify_message(addr, 1, msg2, &sig1, &spend_vk).is_err());
+
+        // ... and the signatures are unique but trivially linkable by the nullifier
+        assert_ne!(&sig1.to_bytes()[..], &sig2.to_bytes()[..]);
+        assert_eq!(sig1.nullifier, sig2.nullifier);
+
+        // Generate a signature with a diversified address
+        let addr_b = {
+            let diversifier = Diversifier([5; 11]);
+            expsk
+                .proof_generation_key()
+                .to_viewing_key()
+                .to_payment_address(diversifier)
+                .unwrap()
+        };
+        let sig1_b = sign_message(&expsk, addr_b, 1, msg1, &params);
+
+        // The signatures are bound to the specific address they were created with
+        assert!(verify_message(addr_b, 1, msg1, &sig1_b, &spend_vk).is_ok());
+        assert!(verify_message(addr_b, 1, msg1, &sig1, &spend_vk).is_err());
+        assert!(verify_message(addr, 1, msg1, &sig1_b, &spend_vk).is_err());
+
+        // ... and the signatures are unlinkable
+        assert_ne!(&sig1.to_bytes()[..], &sig1_b.to_bytes()[..]);
+        assert_ne!(sig1.nullifier, sig1_b.nullifier);
+    }
+}
