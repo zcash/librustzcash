40fc9baac5d2f1e5a84bdb645defb60a9625eafb
Eirik Ogilvie-Wigley <eirik@z.cash>
2019-08-16 22:52:54 -0600

    cargo fix --edition for ff

--- src/lib.rs
+++ src/lib.rs
@@ -266,7 +266,7 @@ pub trait PrimeField: Field {
     }
 
     /// Convert this prime field element into a biginteger representation.
-    fn from_repr(Self::Repr) -> Result<Self, PrimeFieldDecodingError>;
+    fn from_repr(_: Self::Repr) -> Result<Self, PrimeFieldDecodingError>;
 
     /// Convert a biginteger representation into a prime field element, if
     /// the number is an element of the field.
