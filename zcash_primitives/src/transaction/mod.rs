cd1083e086782c0b69d80ad2ead468f982c25ef7
Aditya Kulkarni <adityapk@gmail.com>
2019-10-09 11:50:48 -0700

    Implememt sorting for TxIDs

--- zcash_primitives/src/transaction/mod.rs
+++ zcash_primitives/src/transaction/mod.rs
@@ -24,7 +24,7 @@ const OVERWINTER_TX_VERSION: u32 = 3;
 const SAPLING_VERSION_GROUP_ID: u32 = 0x892F2085;
 const SAPLING_TX_VERSION: u32 = 4;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, Debug, PartialOrd, Ord, PartialEq, Eq, Hash)]
 pub struct TxId(pub [u8; 32]);
 
 impl fmt::Display for TxId {
