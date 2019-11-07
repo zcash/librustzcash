7682a5ae2b927b9d27f287519ada5784e0996b0f
Jack Grigg <jack@z.cash>
2019-11-07 12:11:16 +0000

    Demo WTP parsing

--- zcash_primitives/src/wtp.rs
+++ zcash_primitives/src/wtp.rs
@@ -1,9 +1,12 @@
 //! Core traits and structs for Whitelisted Transparent Programs.
 
 use byteorder::{ReadBytesExt, WriteBytesExt};
+use std::convert::TryInto;
 use std::io::{self, Read, Write};
 
 use crate::serialize::{CompactSize, Vector};
+
+pub(crate) mod demo;
 
 pub trait ToPayload {
     /// Returns a serialized payload and its corresponding mode.
@@ -12,12 +15,14 @@ pub trait ToPayload {
 
 /// The set of programs that have assigned type IDs within the Zcash ecosystem.
 pub enum ProgramType {
+    Demo,
     Unknown(usize),
 }
 
 impl From<usize> for ProgramType {
     fn from(t: usize) -> Self {
         match t {
+            0 => ProgramType::Demo,
             n => ProgramType::Unknown(n),
         }
     }
@@ -26,6 +31,7 @@ impl From<usize> for ProgramType {
 impl From<ProgramType> for usize {
     fn from(type_id: ProgramType) -> usize {
         match type_id {
+            ProgramType::Demo => 0,
             ProgramType::Unknown(n) => n,
         }
     }
@@ -34,6 +40,7 @@ impl From<ProgramType> for usize {
 /// A condition that can be used to encumber transparent funds.
 #[derive(Debug)]
 pub enum Predicate {
+    Demo(demo::Predicate),
     /// A predicate for an unknown program type. This allows the current parser to parse
     /// future transactions containing new program types, while ensuring that they cannot
     /// be considered valid.
@@ -51,6 +58,12 @@ impl Predicate {
         let payload = Vector::read(&mut reader, |r| r.read_u8())?;
 
         match type_id.into() {
+            ProgramType::Demo => {
+                let predicate = (mode, &payload)
+                    .try_into()
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+                Ok(Predicate::Demo(predicate))
+            }
             ProgramType::Unknown(type_id) => Ok(Predicate::Unknown {
                 type_id,
                 mode,
@@ -67,6 +80,10 @@ impl Predicate {
         };
 
         match self {
+            Predicate::Demo(w) => {
+                let (mode, payload) = w.to_payload();
+                inner(writer, ProgramType::Demo, mode, &payload)
+            }
             Predicate::Unknown {
                 type_id,
                 mode,
@@ -79,6 +96,7 @@ impl Predicate {
 /// Data that satisfies the program for prior encumbered funds, enabling them to be spent.
 #[derive(Debug)]
 pub enum Witness {
+    Demo(demo::Witness),
     /// A witness for an unknown program type. This allows the current parser to parse
     /// future transactions containing new program types, while ensuring that they cannot
     /// be considered valid.
@@ -96,6 +114,12 @@ impl Witness {
         let payload = Vector::read(&mut reader, |r| r.read_u8())?;
 
         match type_id.into() {
+            ProgramType::Demo => {
+                let witness = (mode, &payload)
+                    .try_into()
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+                Ok(Witness::Demo(witness))
+            }
             ProgramType::Unknown(type_id) => Ok(Witness::Unknown {
                 type_id,
                 mode,
@@ -112,6 +136,10 @@ impl Witness {
         };
 
         match self {
+            Witness::Demo(w) => {
+                let (mode, payload) = w.to_payload();
+                inner(writer, ProgramType::Demo, mode, &payload)
+            }
             Witness::Unknown {
                 type_id,
                 mode,
