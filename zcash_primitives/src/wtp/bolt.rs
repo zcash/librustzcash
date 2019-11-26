865853afcd8d408c47f1e7918c7f92f9b5451ba7
J. Ayo Akinyele <ayo@boltlabs.io>
2019-11-26 13:01:07 -0500

    initial predicate/witness parsing for Bolt WTPs

--- zcash_primitives/src/wtp/bolt.rs
+++ zcash_primitives/src/wtp/bolt.rs
@@ -58,15 +58,46 @@ mod open {
 }
 
 mod close {
+    use super::convert_u32_to_bytes;
+
     pub const MODE: usize = 1;
 
     #[derive(Debug, PartialEq)]
     pub struct Predicate {
-        pub hash: [u8; 32]
+        pub wpk: [u8; 32],
+        pub cust_bal: u32,
+        pub merch_bal: u32,
+        pub channel_token: Vec<u8> // (pkc, pkm, pkM, mpk) - approx 786
     }
 
     #[derive(Debug, PartialEq)]
-    pub struct Witness(pub [u8; 32]);
+    pub struct Witness {  // (pub [u8; 32]);
+        pub witness_type: u8, // 1 byte
+        pub address: [u8; 32], // 32 bytes
+        pub signature: Vec<u8>, // 72 bytes (cust-sig or merch-sig)
+        pub revoke_token: Vec<u8>, // 104 (wpk + rev-sig)
+    }
+
+    pub fn get_predicate_payload(p: &Predicate) -> Vec<u8> {
+        let mut output = Vec::new();
+        output.extend(p.wpk.iter());
+        output.extend(convert_u32_to_bytes(p.cust_bal).iter());
+        output.extend(convert_u32_to_bytes(p.merch_bal).iter());
+        output.extend(p.channel_token.iter());
+        return output;
+    }
+
+    pub fn get_witness_payload(w: &Witness) -> Vec<u8> {
+        let mut output = Vec::new();
+        output.push(w.witness_type);
+        output.extend(w.address.iter());
+        output.extend(w.signature.iter());
+        if w.witness_type == 0x1 {
+            output.extend(w.revoke_token.iter());
+        }
+        output.push(0x0); // add null as last byte
+        return output;
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -84,8 +115,17 @@ impl Predicate {
         Predicate::Open(open::Predicate { address, channel_token })
     }
 
-    pub fn close(hash: [u8; 32]) -> Self {
-        Predicate::Close(close::Predicate { hash })
+    pub fn close(input: [u8; 1024]) -> Self {
+        let mut channel_token = Vec::new();
+        let mut wpk = [0; 32];
+        wpk.copy_from_slice(&input[0..32]);
+
+        let cust_bal = convert_bytes_to_u32(input[32..36].to_vec());
+        let merch_bal = convert_bytes_to_u32(input[36..40].to_vec());
+
+        channel_token.extend(input[40..].iter());
+
+        Predicate::Close(close::Predicate { wpk, cust_bal, merch_bal, channel_token })
     }
 }
 
@@ -104,17 +144,22 @@ impl TryFrom<(usize, &[u8])> for Predicate {
                     let op = open::Predicate { address, channel_token };
                     Ok(Predicate::Open(op))
                 } else {
-                    Err("Payload is not 33 bytes")
+                    Err("Payload is not 1024 bytes")
                 }
             }
             close::MODE => {
-                if payload.len() == 32 {
-                    let mut hash = [0; 32];
-                    hash.copy_from_slice(&payload);
-                    let cl = close::Predicate { hash };
+                if payload.len() == 1024 {
+                    let mut wpk = [0; 32];
+                    wpk.copy_from_slice(&payload[0..32]);
+                    let cust_bal = convert_bytes_to_u32(payload[32..36].to_vec());
+                    let merch_bal = convert_bytes_to_u32(payload[36..40].to_vec());
+                    let mut channel_token = Vec::new();
+                    channel_token.extend(payload[40..].iter());
+
+                    let cl = close::Predicate { wpk, cust_bal, merch_bal, channel_token };
                     Ok(Predicate::Close(cl))
                 } else {
-                    Err("Payload is not 32 bytes")
+                    Err("Payload is not 1024 bytes")
                 }
             }
             _ => Err("Invalid mode"),
@@ -146,7 +191,7 @@ impl ToPayload for Predicate {
     fn to_payload(&self) -> (usize, Vec<u8>) {
         match self {
             Predicate::Open(p) => (open::MODE, open::get_predicate_payload(p)),
-            Predicate::Close(p) => (close::MODE, p.hash.to_vec()),
+            Predicate::Close(p) => (close::MODE, close::get_predicate_payload(p)),
         }
     }
 }
@@ -175,7 +220,7 @@ pub enum Witness {
     Close(close::Witness),
 }
 
-fn parse_witness_input(input: [u8; 210]) -> open::Witness {
+fn parse_open_witness_input(input: [u8; 210]) -> open::Witness {
     let witness_type = input[0];
     let cust_bal = convert_bytes_to_u32(input[1..5].to_vec());
     let merch_bal = convert_bytes_to_u32(input[5..9].to_vec());
@@ -201,13 +246,34 @@ fn parse_witness_input(input: [u8; 210]) -> open::Witness {
     };
 }
 
+fn parse_close_witness_input(input: [u8; 210]) -> close::Witness {
+    let witness_type = input[0];
+    let mut address= [0u8; 32];
+    let mut signature = Vec::new();
+    let mut revoke_token = Vec::new();
+
+    address.copy_from_slice(&input[1..33]);
+    // cust-sig or merch-sig (depending on witness type)
+    signature.extend_from_slice(&input[33..72].to_vec());
+
+    if witness_type == 0x1 {
+        revoke_token.extend_from_slice(&input[72..209].to_vec());
+    }
+    return close::Witness {
+        witness_type,
+        address,
+        signature,
+        revoke_token
+    };
+}
+
 impl Witness {
     pub fn open(input: [u8; 210]) -> Self {
-        Witness::Open(parse_witness_input(input))
+        Witness::Open(parse_open_witness_input(input))
     }
 
-    pub fn close(preimage: [u8; 32]) -> Self {
-        Witness::Close(close::Witness(preimage))
+    pub fn close(input: [u8; 210]) -> Self {
+        Witness::Close(parse_close_witness_input(input))
     }
 }
 
@@ -224,19 +290,24 @@ impl TryFrom<(usize, &[u8])> for Witness {
                     }
                     let mut witness_input = [0; 210];
                     witness_input.copy_from_slice(payload);
-                    let witness = parse_witness_input(witness_input);
+                    let witness = parse_open_witness_input(witness_input);
                     Ok(Witness::Open(witness))
                 } else {
                     Err("Payload is not 210 bytes")
                 }
             }
             close::MODE => {
-                if payload.len() == 32 {
-                    let mut preimage = [0; 32];
-                    preimage.copy_from_slice(&payload);
-                    Ok(Witness::Close(close::Witness(preimage)))
+                if payload.len() == 210 {
+                    let witness_type = payload[0];
+                    if witness_type != 0x0 && witness_type != 0x1 {
+                        return Err("Invalid witness for close channel mode");
+                    }
+                    let mut witness_input = [0; 210];
+                    witness_input.copy_from_slice(payload);
+                    let witness = parse_close_witness_input(witness_input);
+                    Ok(Witness::Close(witness))
                 } else {
-                    Err("Payload is not 32 bytes")
+                    Err("Payload is not 210 bytes")
                 }
             }
             _ => Err("Invalid mode"),
@@ -268,7 +339,7 @@ impl ToPayload for Witness {
     fn to_payload(&self) -> (usize, Vec<u8>) {
         match self {
             Witness::Open(w) => (open::MODE, open::get_witness_payload(w)),
-            Witness::Close(w) => (close::MODE, w.0.to_vec()),
+            Witness::Close(w) => (close::MODE, close::get_witness_payload(w)),
         }
     }
 }
@@ -279,7 +350,7 @@ mod tests {
 
     use super::{close, open, Predicate, Witness};
     use crate::wtp::ToPayload;
-    use crate::wtp::bolt::parse_witness_input;
+    use crate::wtp::bolt::{parse_open_witness_input, parse_close_witness_input, convert_bytes_to_u32};
 
     #[test]
     fn predicate_open_round_trip() {
@@ -295,15 +366,22 @@ mod tests {
         assert_eq!(p.to_payload(), (open::MODE, data));
     }
 
-//    #[test]
-//    fn predicate_close_round_trip() {
-//        let data = vec![7; 32];
-//        let p: Predicate = (close::MODE, &data[..]).try_into().unwrap();
-//        let hash = [7; 32];
-//        assert_eq!(p, Predicate::Close(close::Predicate { hash }));
-//        assert_eq!(p.to_payload(), (close::MODE, data));
-//    }
-//
+    #[test]
+    fn predicate_close_round_trip() {
+        let data = vec![7; 1024];
+        let p: Predicate = (close::MODE, &data[..]).try_into().unwrap();
+        let mut wpk = [0; 32];
+        wpk.copy_from_slice(&data[0..32]);
+        let cust_bal = convert_bytes_to_u32(data[32..36].to_vec());
+        let merch_bal = convert_bytes_to_u32(data[36..40].to_vec());
+
+        let mut channel_token = Vec::new();
+        channel_token.extend(data[40..].iter());
+
+        assert_eq!(p, Predicate::Close(close::Predicate { wpk, cust_bal, merch_bal, channel_token }));
+        assert_eq!(p.to_payload(), (close::MODE, data));
+    }
+
     #[test]
     fn predicate_rejects_invalid_mode_or_length() {
         for mode in 0..3 {
@@ -323,27 +401,33 @@ mod tests {
         let w: Witness = (open::MODE, &data[..]).try_into().unwrap();
         let mut witness_input = [0; 210];
         witness_input.copy_from_slice(&data);
-        let witness = parse_witness_input(witness_input);
+        let witness = parse_open_witness_input(witness_input);
 
         assert_eq!(w, Witness::Open(witness));
         assert_eq!(w.to_payload(), (open::MODE, data));
     }
 
-//    #[test]
-//    fn witness_close_round_trip() {
-//        let data = vec![7; 32];
-//        let p: Witness = (close::MODE, &data[..]).try_into().unwrap();
-//        assert_eq!(p, Witness::Close(close::Witness([7; 32])));
-//        assert_eq!(p.to_payload(), (close::MODE, data));
-//    }
-//
-//    #[test]
-//    fn witness_rejects_invalid_mode_or_length() {
-//        for mode in 0..3 {
-//            for len in &[32, 32] {
-//                let p: Result<Witness, _> = (mode, &vec![7; *len]).try_into();
-//                assert!(p.is_err());
-//            }
-//        }
-//    }
+    #[test]
+    fn witness_close_round_trip() {
+        let mut data = vec![7; 208];
+        data.insert(0, 0x1);
+        data.push(0x0);
+        let w: Witness = (close::MODE, &data[..]).try_into().unwrap();
+        let mut witness_input = [0; 210];
+        witness_input.copy_from_slice(&data);
+        let witness = parse_close_witness_input(witness_input);
+
+        assert_eq!(w, Witness::Close(witness));
+        assert_eq!(w.to_payload(), (close::MODE, data));
+    }
+
+    #[test]
+    fn witness_rejects_invalid_mode_or_length() {
+        for mode in 0..3 {
+            for len in &[32, 32] {
+                let p: Result<Witness, _> = (mode, &vec![7; *len * 2]).try_into();
+                assert!(p.is_err());
+            }
+        }
+    }
 }
