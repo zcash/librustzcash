26be46573ee7e0094f19d861fd29793adda6298e
NikVolf <nikvolf@gmail.com>
2019-11-28 10:31:16 +0300

    add hash of the node method

--- src/node_data.rs
+++ src/node_data.rs
@@ -193,6 +193,14 @@ impl NodeData {
         let mut cursor = std::io::Cursor::new(buf);
         Self::read(consensus_branch_id, &mut cursor)
     }
+
+    pub fn hash(&self) -> [u8; 32] {
+        let mut buf = [0u8; 32];
+
+        let bytes = self.to_bytes();
+
+        blake2b_personal(&personalization(self.consensus_branch_id), &bytes)
+    }
 }
 
 #[cfg(test)]
