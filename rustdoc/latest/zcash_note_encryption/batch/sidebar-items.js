4a7beda71d94baffbdf00c96cead29cdd6f3249a
str4d <str4d@users.noreply.github.com>
2021-08-10 23:49:57 +0000

    deploy: 13b023387bafdc7b5712c933dc0e16ee94b96a6a

---
+++ rustdoc/latest/zcash_note_encryption/batch/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["try_compact_note_decryption","Trial decryption of a batch of notes for light clients with a set of recipients."],["try_note_decryption","Trial decryption of a batch of notes with a set of recipients."]]});
