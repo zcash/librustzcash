fdc510ae080efbd930084b4ceaca55614a40f228
nuttycom <nuttycom@users.noreply.github.com>
2021-09-14 22:18:01 +0000

    deploy: a3df9dd47d6c3fa89b08483cb8276515464de321

---
+++ rustdoc/latest/implementors/memuse/trait.DynamicUsage.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["zcash_primitives"] = [{"text":"impl DynamicUsage for <a class=\"struct\" href=\"zcash_primitives/transaction/components/amount/struct.Amount.html\" title=\"struct zcash_primitives::transaction::components::amount::Amount\">Amount</a>","synthetic":false,"types":["zcash_primitives::transaction::components::amount::Amount"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
