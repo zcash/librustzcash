d511a788c4b886b5c722a0893c47f0f856a6b6cd
Jack Grigg <jack@electriccoin.co>
2021-12-17 14:57:17 +0000

    zcash_note_encryption: Crate documentation

--- components/zcash_note_encryption/src/lib.rs
+++ components/zcash_note_encryption/src/lib.rs
@@ -1,7 +1,16 @@
-//! Implementation of in-band secret distribution abstractions
-//! for Zcash transactions. The implementations here provide
-//! functionality that is shared between the Sapling and Orchard
-//! protocols.
+//! Note encryption for Zcash transactions.
+//!
+//! This crate implements the [in-band secret distribution scheme] for the Sapling and
+//! Orchard protocols. It provides reusable methods that implement common note encryption
+//! and trial decryption logic, and enforce protocol-agnostic verification requirements.
+//!
+//! Protocol-specific logic is handled via the [`Domain`] trait. Implementations of this
+//! trait are provided in the [`zcash_primitives`] (for Sapling) and [`orchard`] crates;
+//! users with their own existing types can similarly implement the trait themselves.
+//!
+//! [in-band secret distribution scheme]: https://zips.z.cash/protocol/protocol.pdf#saplingandorchardinband
+//! [`zcash_primitives`]: https://crates.io/crates/zcash_primitives
+//! [`orchard`]: https://crates.io/crates/orchard
 
 #![no_std]
 #![cfg_attr(docsrs, feature(doc_cfg))]
