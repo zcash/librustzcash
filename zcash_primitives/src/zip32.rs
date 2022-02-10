970b6d6750710f5f382e82b164aee47aa6f000f5
Kris Nuttycombe <kris@nutty.land>
2022-02-10 09:12:18 -0700

    Add missing trait impls for AccountId

--- zcash_primitives/src/zip32.rs
+++ zcash_primitives/src/zip32.rs
@@ -26,12 +26,18 @@ pub const ZIP32_SAPLING_FVFP_PERSONALIZATION: &[u8; 16] = b"ZcashSaplingFVFP";
 pub const ZIP32_SAPLING_INT_PERSONALIZATION: &[u8; 16] = b"Zcash_SaplingInt";
 
 /// A type-safe wrapper for account identifiers.
-#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Default, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct AccountId(pub u32);
 
 impl From<u32> for AccountId {
     fn from(id: u32) -> Self {
         Self(id)
+    }
+}
+
+impl Into<u32> for AccountId {
+    fn into(self) -> u32 {
+        self.0
     }
 }
 
