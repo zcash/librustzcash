17a4830c703633506acd236d4de72d68c5610944
Jack Grigg <jack@electriccoin.co>
2022-09-16 17:24:14 +0000

    zcash_client_backend: Add gRPC bindings behind feature flag
    The lightwalletd service file is sourced from:
      Git: https://github.com/zcash/lightwalletd
      Rev: ad5ecda5fbb5e12799f926b8cd785d54fdd250c8
    
    Closes zcash/librustzcash#585.

--- zcash_client_backend/build.rs
+++ zcash_client_backend/build.rs
@@ -5,22 +5,22 @@ use std::path::{Path, PathBuf};
 
 const COMPACT_FORMATS_PROTO: &str = "proto/compact_formats.proto";
 
-fn main() -> io::Result<()> {
-    // We don't include the proto files in releases so that downstreams do not need to
-    // regenerate the bindings even if protoc is present.
-    if Path::new(COMPACT_FORMATS_PROTO).exists() {
-        println!("cargo:rerun-if-changed={}", COMPACT_FORMATS_PROTO);
+#[cfg(feature = "lightwalletd-tonic")]
+const SERVICE_PROTO: &str = "proto/service.proto";
 
-        // We check for the existence of protoc in the same way as prost_build, so that people
-        // building from source do not need to have protoc installed. If they make changes to
-        // the proto files, the discrepancy will be caught by CI.
-        if env::var_os("PROTOC")
+fn main() -> io::Result<()> {
+    // - We don't include the proto files in releases so that downstreams do not need to
+    //  regenerate the bindings even if protoc is present.
+    // - We check for the existence of protoc in the same way as prost_build, so that
+    //   people building from source do not need to have protoc installed. If they make
+    //   changes to the proto files, the discrepancy will be caught by CI.
+    if Path::new(COMPACT_FORMATS_PROTO).exists()
+        && env::var_os("PROTOC")
             .map(PathBuf::from)
             .or_else(|| which::which("protoc").ok())
             .is_some()
-        {
-            build()?;
-        }
+    {
+        build()?;
     }
 
     Ok(())
@@ -31,13 +31,47 @@ fn build() -> io::Result<()> {
         .expect("Cannot find OUT_DIR environment variable")
         .into();
 
-    prost_build::compile_protos(&[COMPACT_FORMATS_PROTO], &["proto/"])?;
+    // Build the compact format types.
+    tonic_build::compile_protos(COMPACT_FORMATS_PROTO)?;
 
-    // Copy the generated files into the source tree so changes can be committed.
+    // Copy the generated types into the source tree so changes can be committed.
     fs::copy(
         out.join("cash.z.wallet.sdk.rpc.rs"),
         "src/proto/compact_formats.rs",
     )?;
+
+    #[cfg(feature = "lightwalletd-tonic")]
+    {
+        // Build the gRPC types and client.
+        tonic_build::configure()
+            .build_server(false)
+            .extern_path(
+                ".cash.z.wallet.sdk.rpc.CompactBlock",
+                "crate::proto::compact_formats::CompactBlock",
+            )
+            .extern_path(
+                ".cash.z.wallet.sdk.rpc.CompactTx",
+                "crate::proto::compact_formats::CompactTx",
+            )
+            .extern_path(
+                ".cash.z.wallet.sdk.rpc.CompactSaplingSpend",
+                "crate::proto::compact_formats::CompactSaplingSpend",
+            )
+            .extern_path(
+                ".cash.z.wallet.sdk.rpc.CompactSaplingOutput",
+                "crate::proto::compact_formats::CompactSaplingOutput",
+            )
+            .extern_path(
+                ".cash.z.wallet.sdk.rpc.CompactOrchardAction",
+                "crate::proto::compact_formats::CompactOrchardAction",
+            )
+            .compile(&[SERVICE_PROTO], &["proto/"])?;
+
+        // Copy the generated types into the source tree so changes can be committed. The
+        // file has the same name as for the compact format types because they have the
+        // same package, but we've set things up so this only contains the service types.
+        fs::copy(out.join("cash.z.wallet.sdk.rpc.rs"), "src/proto/service.rs")?;
+    }
 
     Ok(())
 }
