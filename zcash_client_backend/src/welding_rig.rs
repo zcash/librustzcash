77d6ef2fd8532c306c757f237c531eb96fc51e62
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2022-12-09 15:00:46 -0700

    Update zcash_client_backend/src/welding_rig.rs
    Co-authored-by: str4d <thestr4d@gmail.com>

--- zcash_client_backend/src/welding_rig.rs
+++ zcash_client_backend/src/welding_rig.rs
@@ -191,7 +191,6 @@ pub(crate) fn add_block_to_runner<P, S, T>(
 }
 
 #[tracing::instrument(skip_all, fields(height = block.height))]
-#[allow(clippy::suspicious_else_formatting)]
 pub(crate) fn scan_block_with_runner<
     P: consensus::Parameters + Send + 'static,
     K: ScanningKey,
