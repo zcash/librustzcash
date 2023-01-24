6183ec47b87654845cf0c679f22b620ab6452164
Paul <lauxpaul@protonmail.com>
2023-01-24 13:14:49 +0200

    fixed benches/noted_decryption

--- zcash_client_backend/src/proto.rs
+++ zcash_client_backend/src/proto.rs
@@ -9,8 +9,8 @@ use zcash_primitives::{
     transaction::{components::sapling, TxId},
 };
 
-use zcash_note_encryption::{EphemeralKeyBytes};
 use orchard::note_encryption_v3::COMPACT_NOTE_SIZE_V3 as COMPACT_NOTE_SIZE;
+use zcash_note_encryption::EphemeralKeyBytes;
 
 pub mod compact_formats;
 
