cf31990e82f56d2f1e04ff13e7cc31a928ce5ace
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2023-11-23 13:05:23 +0100

    Update zcash_primitives/src/consensus.rs
    Co-authored-by: str4d <thestr4d@gmail.com>

--- zcash_primitives/src/consensus.rs
+++ zcash_primitives/src/consensus.rs
@@ -212,7 +212,7 @@ impl Parameters for MainNetwork {
             NetworkUpgrade::Heartwood => Some(BlockHeight(903_000)),
             NetworkUpgrade::Canopy => Some(BlockHeight(1_046_400)),
             NetworkUpgrade::Nu5 => Some(BlockHeight(1_687_104)),
-            NetworkUpgrade::Nu6 => Some(BlockHeight(2_726_400)),
+            NetworkUpgrade::Nu6 => None,
             #[cfg(feature = "zfuture")]
             NetworkUpgrade::ZFuture => None,
         }
