e7f71c3f1fb5e6193977918c775ba29220b6346c
Jack Grigg <jack@electriccoin.co>
2023-12-06 18:23:44 +0000

    Revert "Remove sapling-crypto"
    This reverts commit 1b865ecfdf3d6743fa2e044b77f273db0e28cb9d.

---
+++ sapling-crypto/src/lib.rs
@@ -0,0 +1,18 @@
+extern crate pairing;
+extern crate bellman;
+extern crate blake2b_simd;
+extern crate blake2s_simd;
+extern crate digest;
+extern crate ff;
+extern crate rand_core;
+extern crate byteorder;
+
+#[cfg(test)]
+#[macro_use]
+extern crate hex_literal;
+
+#[cfg(test)]
+extern crate rand_xorshift;
+
+#[cfg(test)]
+extern crate sha2;
