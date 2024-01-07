30436845adaafd466c939cd807833f90dbf02423
teor <teor@riseup.net>
2024-01-08 08:16:02 +1000

    Add an optional solver feature

--- components/equihash/build.rs
+++ components/equihash/build.rs
@@ -1,11 +1,13 @@
 //! Build script for the equihash tromp solver in C.
 
 fn main() {
+    #[cfg(feature = "solver")]
     cc::Build::new()
         .include("tromp/")
         .file("tromp/equi_miner.c")
         .compile("equitromp");
 
     // Tell Cargo to only rerun this build script if the tromp C files or headers change.
+    #[cfg(feature = "solver")]
     println!("cargo:rerun-if-changed=tromp");
 }
