5f77bd79dbaa85285418c9e21cbd7f0fdbf0c21d
teor <teor@riseup.net>
2024-01-08 08:16:02 +1000

    equihash: Place solver behind a feature flag

--- components/equihash/build.rs
+++ components/equihash/build.rs
@@ -1,6 +1,12 @@
 //! Build script for the equihash tromp solver in C.
 
 fn main() {
+    #[cfg(feature = "solver")]
+    build_tromp_solver();
+}
+
+#[cfg(feature = "solver")]
+fn build_tromp_solver() {
     cc::Build::new()
         .include("tromp/")
         .file("tromp/equi_miner.c")
