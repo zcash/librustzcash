bf18a03a1ba8742d03a86a7add031bf4ae71ea12
daira <daira@users.noreply.github.com>
2024-12-07 17:43:58 +0000

    deploy: 37253132fd2f50c526ae8b711d77998739efa043

--- rustdoc/latest/zcash_client_backend/data_api/testing/transparent/sidebar-items.js
+++ rustdoc/latest/zcash_client_backend/data_api/testing/transparent/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["put_received_transparent_utxo","transparent_balance_across_shielding"]};
+window.SIDEBAR_ITEMS = {"fn":["put_received_transparent_utxo","transparent_balance_across_shielding","transparent_balance_spendability"]};
