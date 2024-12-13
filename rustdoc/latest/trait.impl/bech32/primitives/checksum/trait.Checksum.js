1d9a75fc67cb37074708189e71dd40e5c08bc5fb
nuttycom <nuttycom@users.noreply.github.com>
2024-12-13 21:02:25 +0000

    deploy: 82093f87e7cbfe9e773e9b9f2a87a76553046222

---
+++ rustdoc/latest/trait.impl/bech32/primitives/checksum/trait.Checksum.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["zcash_address",[["impl Checksum for <a class=\"enum\" href=\"zcash_address/unified/enum.Bech32mZip316.html\" title=\"enum zcash_address::unified::Bech32mZip316\">Bech32mZip316</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[185]}
