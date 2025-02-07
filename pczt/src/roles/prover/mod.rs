a2c3134dbda0e500031a7c8204b0277c82954eda
Jack Grigg <jack@electriccoin.co>
2025-02-07 11:13:21 +0000

    pczt: Add `Prover::{requires_sapling_proofs, requires_orchard_proof}`
    Closes zcash/librustzcash#1696.

--- pczt/src/roles/prover/mod.rs
+++ pczt/src/roles/prover/mod.rs
@@ -20,6 +20,20 @@ impl Prover {
         Self { pczt }
     }
 
+    /// Returns `true` if this PCZT contains Sapling Spends or Outputs that are missing
+    /// proofs.
+    pub fn requires_sapling_proofs(&self) -> bool {
+        let sapling_bundle = self.pczt.sapling();
+
+        sapling_bundle.spends().iter().any(|s| s.zkproof.is_none())
+            || sapling_bundle.outputs().iter().any(|o| o.zkproof.is_none())
+    }
+
+    /// Returns `true` if this PCZT contains Orchard Actions but no Orchard proof.
+    pub fn requires_orchard_proof(&self) -> bool {
+        !self.pczt.orchard().actions().is_empty() && self.pczt.orchard().zkproof.is_none()
+    }
+
     /// Finishes the Prover role, returning the updated PCZT.
     pub fn finish(self) -> Pczt {
         self.pczt
