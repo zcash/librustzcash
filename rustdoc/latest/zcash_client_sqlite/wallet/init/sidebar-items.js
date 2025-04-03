72c4721e523ab88a55934cd28913c87910a7d42e
nuttycom <nuttycom@users.noreply.github.com>
2025-04-03 22:05:01 +0000

    deploy: 330c4d7200cd55eeb36eb1c1ca8d1c5081ca29b0

--- rustdoc/latest/zcash_client_sqlite/wallet/init/sidebar-items.js
+++ rustdoc/latest/zcash_client_sqlite/wallet/init/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["WalletMigrationError"],"fn":["init_wallet_db"]};
+window.SIDEBAR_ITEMS = {"enum":["WalletMigrationError"],"fn":["init_wallet_db"],"mod":["migrations"],"struct":["WalletMigrator"]};
