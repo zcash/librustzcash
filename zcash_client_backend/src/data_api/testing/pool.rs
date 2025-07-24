c766ecaa91fbaf533013b6f04b7f2b3732402a45
Schell Carl Scivally <efsubenovex@gmail.com>
2025-07-24 16:13:55 +1200

    plumb ConfirmationsPolicy into SQL query for spendable notes

--- zcash_client_backend/src/data_api/testing/pool.rs
+++ zcash_client_backend/src/data_api/testing/pool.rs
@@ -2,7 +2,7 @@ use std::{
     cmp::Eq,
     convert::Infallible,
     hash::Hash,
-    num::{NonZeroU64, NonZeroU8, NonZeroUsize},
+    num::{NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize},
 };
 
 use assert_matches::assert_matches;
@@ -2055,13 +2055,18 @@ pub fn checkpoint_gaps<T: ShieldedPoolTester, DSF: DataStoreFactory>(
     // Scan the block
     st.scan_cached_blocks(account.birthday().height() + 10, 1);
 
+    println!("account_birthday_height: {}", account.birthday().height());
     // Verify that our note is considered spendable
     let spendable = T::select_spendable_notes(
         &st,
         account.id(),
         TargetValue::AtLeast(Zatoshis::const_from_u64(300000)),
         account.birthday().height() + 5,
-        ConfirmationsPolicy::default(),
+        ConfirmationsPolicy {
+            // 5
+            untrusted: NonZeroU32::MIN.saturating_add(4),
+            ..Default::default()
+        },
         &[],
     )
     .unwrap();
@@ -3309,7 +3314,7 @@ pub fn wallet_recovery_computes_fees<T: ShieldedPoolTester, DsF: DataStoreFactor
                 &input_selector,
                 &change_strategy,
                 request.clone(),
-                ConfirmationsPolicy::new_symmetrical(1).unwrap(),
+                ConfirmationsPolicy::MIN,
             )
             .unwrap();
         let result0 = st
