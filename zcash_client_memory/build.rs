7b62e0028659700698dbbd8350a0e70f832277b7
Kris Nuttycombe <kris@nutty.land>
2025-08-26 12:53:40 -0600

    zcash_client_memory: Move .proto files out of `src` tree.

--- zcash_client_memory/build.rs
+++ zcash_client_memory/build.rs
@@ -1,16 +1,22 @@
 use std::env;
 use std::fs;
 use std::io;
+use std::path::Path;
 use std::path::PathBuf;
 
+const MEMORY_WALLET_PROTO: &str = "proto/memory_wallet.proto";
+
 fn main() -> io::Result<()> {
+    // - We don't include the proto files in releases so that downstreams do not need to
+    //  regenerate the bindings even if protoc is present.
     // - We check for the existence of protoc in the same way as prost_build, so that
     //   people building from source do not need to have protoc installed. If they make
     //   changes to the proto files, the discrepancy will be caught by CI.
-    if env::var_os("PROTOC")
-        .map(PathBuf::from)
-        .or_else(|| which::which("protoc").ok())
-        .is_some()
+    if Path::new(MEMORY_WALLET_PROTO).exists()
+        && env::var_os("PROTOC")
+            .map(PathBuf::from)
+            .or_else(|| which::which("protoc").ok())
+            .is_some()
     {
         build()?;
     }
@@ -25,13 +31,13 @@ fn build() -> io::Result<()> {
 
     prost_build::compile_protos(
         &[
-            "src/proto/memory_wallet.proto",
-            "src/proto/notes.proto",
-            "src/proto/primitives.proto",
-            "src/proto/shardtree.proto",
-            "src/proto/transparent.proto",
+            MEMORY_WALLET_PROTO,
+            "proto/notes.proto",
+            "proto/primitives.proto",
+            "proto/shardtree.proto",
+            "proto/transparent.proto",
         ],
-        &["src/"],
+        &["proto/"],
     )?;
 
     // Copy the generated types into the source tree so changes can be committed.
