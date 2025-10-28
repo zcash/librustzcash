056cdb1246c5e52ce1a22aeeee4c9de2c95e4d94
daira <daira@users.noreply.github.com>
2025-10-28 19:34:26 +0000

    deploy: 98e153bb92f935e0dd5b171b6bc3a4db118f80b6

--- rustdoc/latest/trait.impl/zcash_client_backend/data_api/chain/trait.BlockCache.js
+++ rustdoc/latest/trait.impl/zcash_client_backend/data_api/chain/trait.BlockCache.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["zcash_client_memory",[["impl BlockCache for <a class=\"struct\" href=\"zcash_client_memory/struct.MemBlockCache.html\" title=\"struct zcash_client_memory::MemBlockCache\">MemBlockCache</a>"]]]]);
+    var implementors = Object.fromEntries([["zcash_client_memory",[["impl <a class=\"trait\" href=\"zcash_client_backend/data_api/chain/trait.BlockCache.html\" title=\"trait zcash_client_backend::data_api::chain::BlockCache\">BlockCache</a> for <a class=\"struct\" href=\"zcash_client_memory/struct.MemBlockCache.html\" title=\"struct zcash_client_memory::MemBlockCache\">MemBlockCache</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[194]}
+//{"start":57,"fragment_lengths":[350]}
