4b122638cdcdbaf98f7221434e2b39c1dc8d760c
Kris Nuttycombe <kris@nutty.land>
2025-12-12 15:10:48 -0700

    zcash_client_memory: Fix broken `--cfg zcash_unstable="zfuture"` build

--- zcash_client_memory/src/wallet_read.rs
+++ zcash_client_memory/src/wallet_read.rs
@@ -548,6 +548,11 @@ impl<P: consensus::Parameters> WalletRead for MemoryWalletDb<P> {
                         BranchId::for_height(&self.params, expiry_height),
                         tx_data.lock_time(),
                         expiry_height,
+                        #[cfg(all(
+                            any(zcash_unstable = "nu7", zcash_unstable = "zfuture"),
+                            feature = "zip-233"
+                        ))]
+                        tx_data.zip233_amount(),
                         tx_data.transparent_bundle().cloned(),
                         tx_data.sprout_bundle().cloned(),
                         tx_data.sapling_bundle().cloned(),
