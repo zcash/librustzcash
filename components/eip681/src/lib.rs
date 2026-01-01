a523daf78c204c7d16da7cb71068bff9fa5d3ce3
Schell Carl Scivally <efsubenovex@gmail.com>
2026-01-02 11:18:01 +1300

    eip681: docs mention core rules of ABNF, EIP-137

--- components/eip681/src/lib.rs
+++ components/eip681/src/lib.rs
@@ -17,6 +17,18 @@
 //! value            = number / ethereum_address / STRING
 //! number           = [ "-" / "+" ] *DIGIT [ "." 1*DIGIT ] [ ( "e" / "E" ) [ 1*DIGIT ] ]
 //! ```
+//!
+//! Where `TYPE` is a standard ABI type name, as defined in Ethereum Contract
+//! ABI specification. `STRING` is a URL-encoded unicode string of arbitrary
+//! length, where delimiters and the percentage symbol (%) are mandatorily
+//! hex-encoded with a % prefix.
+//!
+//! For the syntax of `ENS_NAME`, please consult
+//! [ERC-137](https://eips.ethereum.org/EIPS/eip-137) defining Ethereum Name Service.
+//!
+//! See
+//! [ABNF core rules](https://en.wikipedia.org/wiki/Augmented_Backus%E2%80%93Naur_form#Core_rules)
+//! for general information about the `ABNF` format.
 
 pub mod error;
 mod parse;
