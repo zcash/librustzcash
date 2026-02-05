95e3c475412a361aeb3d0f81817189f937924b24
str4d <str4d@users.noreply.github.com>
2026-02-05 18:57:09 +0000

    deploy: 15b7a43ac57b1daec5d23e88c58da024f60a1776

--- rustdoc/latest/trait.impl/zcash_primitives/transaction/fees/transparent/trait.InputView.js
+++ rustdoc/latest/trait.impl/zcash_primitives/transaction/fees/transparent/trait.InputView.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["zcash_client_backend",[["impl InputView for <a class=\"struct\" href=\"zcash_client_backend/data_api/struct.WalletUtxo.html\" title=\"struct zcash_client_backend::data_api::WalletUtxo\">WalletUtxo</a>",0],["impl InputView for <a class=\"struct\" href=\"zcash_client_backend/wallet/struct.WalletTransparentOutput.html\" title=\"struct zcash_client_backend::wallet::WalletTransparentOutput\">WalletTransparentOutput</a>",0]]],["zcash_primitives",[]]]);
+    const implementors = Object.fromEntries([["zcash_client_backend",[["impl <a class=\"trait\" href=\"zcash_primitives/transaction/fees/transparent/trait.InputView.html\" title=\"trait zcash_primitives::transaction::fees::transparent::InputView\">InputView</a> for <a class=\"struct\" href=\"zcash_client_backend/data_api/struct.WalletUtxo.html\" title=\"struct zcash_client_backend::data_api::WalletUtxo\">WalletUtxo</a>",0],["impl <a class=\"trait\" href=\"zcash_primitives/transaction/fees/transparent/trait.InputView.html\" title=\"trait zcash_primitives::transaction::fees::transparent::InputView\">InputView</a> for <a class=\"struct\" href=\"zcash_client_backend/wallet/struct.WalletTransparentOutput.html\" title=\"struct zcash_client_backend::wallet::WalletTransparentOutput\">WalletTransparentOutput</a>",0]]],["zcash_primitives",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[425,24]}
+//{"start":59,"fragment_lengths":[775,24]}
